db8a5f971a4b6e3e9bdde354f37f6dfb9d18f7e1
yangguang <yangguang@bilibili.com>
2018-03-17 13:18:22 +0800

    金句猫管理后台系统

---
+++ src/components/MainNav.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <div class="header">
+          jinjumao管理后台
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+      return{
+
+      }
+  }
+}
+</script>
+
+<style scoped>
+    .header{
+        background-color: #000;
+        color: #fff;
+        font-size: 30px;
+        padding: 10px 20px;
+    }
+</style>
+
+
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import MainNav from '@/components/MainNav'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'MainNav',
+      component: MainNav
+    }
+  ]
+})
